4ea97535e4fcfe66ab7fee50541402253a86aa00
Toqi Abdullah <toqiabdullah61990@gmail.com>
2026-02-22 00:15:00 +0600

    Register patient functionality completed

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { UserStatus } from "../../../generated/prisma/enums";
 import { auth } from "../../lib/auth";
+import { prisma } from "../../lib/prisma";
 
 interface IRegisterPatientPayload {
     name: string,
@@ -23,7 +24,21 @@ const registerUser = async (payload: IRegisterPatientPayload) => {
     if (!data.user){
         throw new Error("Patient registration failed");
     }
-    return data;
+    const patient = await prisma.$transaction(async (tx) => {
+        const patientTx = await tx.patient.create({
+            data:{
+                userId: data.user.id,
+                name:data.user.name,
+                email:data.user.email,
+                
+            }
+        })
+        return patientTx
+    })
+    return {
+        ...data,
+        patient
+    };
 };
 
 const loginPatient = async (payload: ILoginPatientPayload) => {
